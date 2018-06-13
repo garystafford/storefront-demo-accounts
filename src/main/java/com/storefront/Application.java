0cd1fa3cc76fd41f70ed29202114a60e48640f35
Gary A. Stafford <garystafford@rochester.rr.com>
2018-06-12 22:38:41 -0400

    Add Spring Cloud Eureka Client for service discovery

--- src/main/java/com/storefront/Application.java
+++ src/main/java/com/storefront/Application.java
@@ -2,7 +2,9 @@ package com.storefront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
