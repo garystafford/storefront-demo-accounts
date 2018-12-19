51276728e3ebebd629b40b52df56c035618a80fa
Gary A. Stafford <garystafford@rochester.rr.com>
2018-12-18 21:49:27 -0500

    Add gke branch

--- Docker/docker-build-push.sh
+++ Docker/docker-build-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-accounts:latest .
-docker push garystafford/storefront-accounts:latest
+docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-accounts:gke-2.0.0 .
+docker push garystafford/storefront-accounts:gke-2.0.0
 
-# docker run --name storefront-accounts -d garystafford/storefront-accounts:latest
+# docker run --name storefront-accounts -d garystafford/storefront-accounts:gke-2.0.0
--- src/main/java/com/storefront/Application.java
+++ src/main/java/com/storefront/Application.java
@@ -2,12 +2,10 @@ package com.storefront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * author Gary A. Stafford
  */
-@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
