9214ee1ca3dec8c37d19a9852f767b94a465777b
Gary A. Stafford <garystafford@rochester.rr.com>
2021-05-11 17:48:47 -0400

    Updating for 2021.05 release

--- Docker/docker-build-push.sh
+++ Docker/docker-build-push.sh
@@ -2,8 +2,10 @@
 
 # Usage (from root of project): sh ./Docker/docker-build-push.sh
 
-./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-accounts:gke-3.0.0 .
-docker push garystafford/storefront-accounts:gke-3.0.0
+# docker build -f Docker/Dockerfile_base --no-cache -t garystafford/storefront-base:3.0.0 .
 
-# docker run --name storefront-accounts -d garystafford/storefront-accounts:gke-3.0.0
+./gradlew clean build
+docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-accounts:3.0.0 .
+docker push garystafford/storefront-accounts:3.0.0
+
+# docker run --name storefront-accounts -d garystafford/storefront-accounts:3.0.0
--- src/main/java/com/storefront/Application.java
+++ src/main/java/com/storefront/Application.java
@@ -2,11 +2,13 @@ package com.storefront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * author Gary A. Stafford
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
