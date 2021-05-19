42413359709a022dc497f663555d0cbaf0cb3cee
Gary A. Stafford <garystafford@rochester.rr.com>
2021-05-19 07:33:00 -0400

    Updating for 2021.05 release

--- Docker/docker-build-push.sh
+++ Docker/docker-build-push.sh
@@ -5,7 +5,7 @@
 # docker build -f Docker/Dockerfile_base --no-cache -t garystafford/storefront-base:3.0.0 .
 
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-accounts:3.0.0 .
-docker push garystafford/storefront-accounts:3.0.0
+docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-accounts:3.0.0-minikube .
+docker push garystafford/storefront-accounts:3.0.0-minikube
 
 # docker run --name storefront-accounts -d garystafford/storefront-accounts:3.0.0
--- src/main/java/com/storefront/Application.java
+++ src/main/java/com/storefront/Application.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * author Gary A. Stafford
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
